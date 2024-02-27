d26b18079cb08f3b289e3ea2e7a90d799d528798
Artur Tuzim <artur@tuzim.xyz>
2024-02-27 21:07:04 +0100

    Disabled configuration cache as it is causing problems and updated LocalSkillsDataSource

--- core/local/src/main/kotlin/xyz/dussim/local/impl/LocalSkillsDataSource.kt
+++ core/local/src/main/kotlin/xyz/dussim/local/impl/LocalSkillsDataSource.kt
@@ -1,9 +1,9 @@
 package xyz.dussim.local.impl
 
+import java.util.Comparator.comparing
 import xyz.dussim.api.data.DataSource
 import xyz.dussim.data.skills.Skill
 import xyz.dussim.data.skills.SkillName
-import java.util.Comparator.comparing
 
 internal class LocalSkillsDataSource : DataSource<List<Skill>> by LocalDataSource(STATIC_DATA) {
     companion object {
@@ -13,7 +13,7 @@ internal class LocalSkillsDataSource : DataSource<List<Skill>> by LocalDataSourc
             Skill.proficient(SkillName.Android),
             Skill.advanced(SkillName.Dagger2),
             Skill.advanced(SkillName.Git),
-            Skill.beginner(SkillName.Spring)
+            Skill.competent(SkillName.Ktor)
         ).sortedWith(comparing(Skill::level).reversed())
     }
 }
